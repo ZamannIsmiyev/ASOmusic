e58a4cfabced60d1077ec3308bd2669a5b60b8b0
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-05-19 17:33:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
+BOT_NAME = getenv("BOT_NAME", "ASOMusicbot") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "ASOMusic_bot")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
